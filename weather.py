dedde228c5509242cc6d83c2001a8d15cc3a2d53
YONEKAWA Rintaro <72489316+yrin71@users.noreply.github.com>
2025-11-02 22:21:28 +0900

    Update weather.py

--- weather.py
+++ weather.py
@@ -5,22 +5,22 @@ from bs4 import BeautifulSoup
 
 
 def fetch_tenki_jp():
-    url = "https://tenki.jp/forecast/2/10/3630/7202/"
+    url = "https://tenki.jp/forecast/2/10/3630/7202/"                        #好きな天気予報サイトのURL
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
 
     # 天気
     #weather = soup.find('p', class_='weather-telop')
-    weather = soup.select_one('.weather-telop').get_text(strip = True)
-    
+    weather = soup.select_one('.weather-telop').get_text(strip = True)       #html内の weather-telop クラスに天気が書いてあるので抽出する
+                                                                             #例: 晴れのち曇り
     # 温度
     #temp_max = soup.find('dd', class_='high-temp temp')
     #temp_min = soup.find('dd', class_='low-temp temp')
-    temp = soup.select_one('.date-value').get_text(strip = True).replace('[0]', '').split('℃')
+    temp = soup.select_one('.date-value').get_text(strip = True).replace('[0]', '').split('℃')    #温度を抽出する． サイトに応じてreplaceやre.subを使って数字を抽出する
     temp_max = re.sub(r'\D+', '', temp[0])
     temp_min = re.sub(r'\D+', '', temp[1])
     # 降水確率
-    row = soup.select_one('.rain-probability').get_text(strip = True).replace('降水確率', '').replace('---', '', 3)
+    row = soup.select_one('.rain-probability').get_text(strip = True).replace('降水確率', '').replace('---', '', 3)    #降水確率は24時間の中で最も高いものを抽出
     rain_values = row.split('%')
     rain = max(rain_values)
 
@@ -33,7 +33,7 @@ def fetch_tenki_jp():
 
 
 def notify_discord(msg):
-    requests.post('https://discord.com/api/webhooks/1434390104826187938/W1OFhik2opEo103XL7NNxNkpJ_qTu_Ph-jPWHcDZ678Er3PQzR2flFZir20iqbCiRm-5', json={"content": msg})
+    requests.post('https://discord.com/api/webhooks/__________Discord Hook URL_________, json={"content": msg})    #json形式にしてDiscord Hookに送信
 
 
 def main():
@@ -45,8 +45,8 @@ def main():
         f"最低気温: {data['temp_min']}℃\n"
         f"降水確率: {data['rain']}%"
     )
-
     notify_discord(msg)
     #print(data)
 
-if __name__ == '__main__': main()
+
+if __name__ == '__main__': main()                                             # importでは実行されない. 
